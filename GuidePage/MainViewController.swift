22491142900ff15a88e04fc9c907ce97ace7499a
吕宁锋 <lvningfeng@lvningfengdeMacBook-Pro.local>
2015-12-17 10:37:56 +0800

    引导页

---
+++ GuidePage/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  GuidePage
+//
+//  Created by 吕宁锋 on 15/12/16.
+//  Copyright © 2015年 吕宁锋. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.setHidesBackButton(true, animated: true)
+        
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GuidePage/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  GuidePage
+//
+//  Created by 吕宁锋 on 15/12/16.
+//  Copyright © 2015年 吕宁锋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var guidepage: UIPageControl!
+    @IBOutlet weak var buttonNext: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        buttonNext.hidden=true
+ 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func swipeGesture(sender: UISwipeGestureRecognizer) {
+        guidepage.currentPage=guidepage.currentPage+1
+        if (guidepage.hidden==false)
+        {
+            changeImage()
+        }
+    }
+
+    @IBAction func swipeleft(sender: UISwipeGestureRecognizer) {
+        guidepage.currentPage=guidepage.currentPage-1
+        if (guidepage.hidden==false)
+        {
+            changeImage()
+        }
+    }
+    @IBAction func FuncBTNNext(sender: AnyObject) {
+    }
+
+    @IBAction func GuidePageChanged(sender: AnyObject) {
+        changeImage()
+    }
+    
+    func changeImage()
+    {
+        let image1=UIImage(named: "3.png")
+        let image2=UIImage(named: "123.jpg")
+        let image3=UIImage(named: "5.jpg")
+        let pageNumber=guidepage.currentPage;
+        switch (pageNumber) {
+        case 0:
+            imageView.image=image1
+            break;
+        case 1:
+            imageView.image=image2
+            break;
+        case 2:
+            imageView.image=image3
+            buttonNext.hidden=false
+            guidepage.hidden=true
+            break;
+        default:
+            break;
+        }
+
+    }
+
+}
+
